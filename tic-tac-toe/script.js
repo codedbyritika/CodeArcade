4a634d48a51fb32de1325efab2a89d1e257e0b7f
codedbyritika <ritika181223@gmail.com>
2026-04-20 23:54:38 +0530

    basic game logic

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,1 +1,17 @@
-console.log("welcome to tic tac toe")
+console.log("welcome to tic tac toe");
+let bgmusic=new Audio("./assets/music.mp3");
+let turn=new Audio("./assets/ting.mp3");
+let gameover=new Audio("./assets/gameover.mp3");
+
+
+let turn ="X";
+// X to O or vice versa
+const changeTurn=()=>{
+return turn==="X"?"O":"X"
+}
+
+//function to check if player won the game or not
+
+const checkWin= ()=>{
+    
+}
